856a25a7925b8b7276ad891c7e6098d3fff73b87
Ethan Forvest <91059346+ethanforvest@users.noreply.github.com>
2024-04-04 16:53:07 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/public/vite.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -28,7 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>Hello world!</p>
     </>
   );
 }
